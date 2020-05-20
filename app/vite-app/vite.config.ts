edcc4e30395c84f03b73db5254dc2fda466f7e7c
ushironoko <apple19940820@gmail.com>
2020-05-20 10:36:44 +0900

    コメント追加

--- app/vite-app/vite.config.ts
+++ app/vite-app/vite.config.ts
@@ -2,6 +2,6 @@ import { SharedConfig } from 'vite'
 
 export default {
   optimizeDeps: {
-    exclude: ['@vite-playground/base']
+    // exclude: ['@vite-playground/base'] // depOptimizerを通るとキャッシュに入って使えるようになるがhmrが効かない
   },
 } as SharedConfig
